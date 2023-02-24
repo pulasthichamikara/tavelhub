86ee4a09320b4c9519be43c2bdf641b94728f9b7
pcchamikara <pcchamikara@gmail.com>
2023-02-24 20:11:02 +0530

    git remove unvanterd  code

--- src/index.js
+++ src/index.js
@@ -3,21 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import { UserContexProvider } from './contex/UserContext';
-import { SearchContextProvider } from './contex/SearchContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
-      <UserContexProvider>
-        <SearchContextProvider>
-          <App />
-        </SearchContextProvider>
-      </UserContexProvider>
-    </GoogleOAuthProvider>
+    <UserContexProvider>
+      {/* <App /> */}
+      <>test</>
+    </UserContexProvider>
   </React.StrictMode>
 );
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,8 @@
 import React, { useContext, useEffect } from 'react';
 
 import GridAccomadation from '../components/GridAccomadation';
-import { SearchContext } from '../contex/SearchContext';
 
 export default function Home() {
-  const { setSearchQuery } = useContext(SearchContext);
-
-  useEffect(() => {
-    setSearchQuery({});
-  }, []);
-
   return (
     <div className="">
       <GridAccomadation que={''} />
